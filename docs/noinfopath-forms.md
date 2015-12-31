d22e8f32ed8ca45c84abe2ba051f4b0d6ba0951f
gochinj <jeff@imginconline.com>
2015-12-31 10:51:17 -0500

    Fixed saving bug that was preventing saving of child records.

--- docs/noinfopath-forms.md
+++ docs/noinfopath-forms.md
@@ -1,5 +1,5 @@
 # noinfopath.forms
-@version 1.0.11
+@version 1.0.12
 
 Combines the functionality of validation from bootstrap and angular.
 
--- src/globals.js
+++ src/globals.js
@@ -1,6 +1,6 @@
 /**
 * # noinfopath.forms
-* @version 1.0.11
+* @version 1.0.12
 *
 * Combines the functionality of validation from bootstrap and angular.
 *
