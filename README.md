73dd3b1451d611b5e73383f75174f2a2ca6cdc48
Jeff <jeff@imginconline.com>
2016-09-28 17:39:01 -0400

    bump version to 2.0.8

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # noinfopath.forms
-@version 2.0.7
+@version 2.0.8
 
 Implements the NoInfoPath Transaction processing in conjunction with AngularJS validation mechanism.
 
--- src/globals.js
+++ src/globals.js
@@ -1,6 +1,6 @@
 /**
  * # noinfopath.forms
- * @version 2.0.7
+ * @version 2.0.8
  *
  * Implements the NoInfoPath Transaction processing in conjunction with AngularJS validation mechanism.
  *
