e67a75d6dd94d2ee3a9baba25617d2f921225534
Adarian Eastman <adarian@imginconline.com>
2016-12-22 11:29:25 -0500

    Removed no-parameter-parser.js

--- gruntfile.js
+++ gruntfile.js
@@ -14,8 +14,7 @@ module.exports = function(grunt) {
 					'src/tabs.js',
 					'src/navigation.js',
 					'src/validation.js',
-					'src/form-config.js',
-					'src/parameter-parser.js'
+					'src/form-config.js'
 				],
 				dest: 'dist/noinfopath-forms.js'
 			},
