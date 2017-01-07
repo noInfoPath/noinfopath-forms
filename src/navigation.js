8961d468567f90120240d7d86dfd4e0ee10334a0
Jeffrey A. Gochin <jeff@imginconline.com>
2017-01-07 11:58:02 -0500

    Documenting noNavigation

--- src/navigation.js
+++ src/navigation.js
@@ -28,6 +28,8 @@
 					}
 				}
 
+				btn.click(_click.bind(ctx, ctx, comp, scope, el));
+
 				return btn;
 			},
 			"message": function (ctx, cfg, scope, el) {
@@ -76,6 +78,28 @@
 			}
 		}
 
+		function _registerWatch(ctx, scope, el, uid) {
+			var scopeKey = uid ? ctx.component.scopeKey + "_" + uid : ctx.component.scopeKey;
+
+			noInfoPath.setItem(scope, "noNavigation." + scopeKey + ".currentNavBar", ctx.component.default);
+
+			/*
+			*	noNavigation sets up an AngularJS watch on the specific rows property
+			*	on the scope, and also captures and stores the watch's unregister method
+			*	for later use.
+			*
+			*	When the currentNavBar value  changes for a given row the
+			*	`noKendoHelpers.changeRowNavBarWatch` method is called to handle
+			*	the event.
+			*/
+			if(uid) {
+				var unRegWatch = scope.$watch("noNavigation." + scopeKey + ".currentNavBar", noKendoHelpers.changeRowNavBarWatch.bind(ctx, ctx, scope, el));
+
+				noInfoPath.setItem(scope, "noNavigation." + scopeKey + ".deregister", unRegWatch);
+			}
+
+		}
+
 		function _compile(el, attrs) {
 			var ctx = noFormConfig.getComponentContextByRoute($state.current.name, undefined, "noNavigation", attrs.noForm);
 			//el.attr("noid", noInfoPath.createNoid());
@@ -88,45 +112,130 @@
 
 			el.empty();
 
+			/*
+			*	#### @property noNavigation::bars
+			*
+			*	Creates a new `<navbar>` element for each navbar found in the
+			*	no-forms noNavigation.bars configuration node.
+			*/
 			for(var b in bars) {
 				var bar = bars[b],
 					btnBar = angular.element("<navbar></navbar>");
-
+				/*
+				*	##### Sample navbar configuration
+				*
+				*	```json
+				*	"quantities": {
+				*		"ngShow": "!project.$dirty",
+				*		"class": "no-flex justify-left no-flex-item size-1",
+				*		"components": [
+				*			{
+				*				"type": "button",
+				*				"actions": [{
+				*					"provider": "$state",
+				*					"method": "go",
+				*					"params": ["efr.client.search"],
+				*					"noContextParams": true
+				*				}],
+				*				"class": "btn btn-default no-flex-item",
+				*				"icon": {
+				*					"class": "glyphicon glyphicon-arrow-left no-text"
+				*				}
+				*			}
+				*		]
+				*	}
+				*	```
+				*
+				*	When a bar configuration is a string then it is an alias
+				*	or a reference to another bar configuration.
+				*/
 				if(angular.isString(bar)){
 					bar = bars[bar];  //Aliased
 				}
 
+				/*
+				*	This directive adds a `bar-id` attribute to the `<navbar>` element using
+				*	each bars configuration key.
+				*/
 				btnBar.attr("bar-id", b);
 
+				/*
+				*	#### @property noNavigation::bar::class
+				*
+				*	It then appends any CSS class defined in the configuration.
+				*/
 				btnBar.addClass(bar.class);
 
+				/*
+				*	#### @property noNavigation::default
+				*
+				*	noNavigation allows multiple bars to be defined, one bar is
+				*	always defined as the default. So if a bar is not the default
+				*	then add the AngularJS directive `ng-hide`.
+				*/
 				if(b !== ctx.component.default) {
 					btnBar.addClass("ng-hide");
 				}
 
-
+				/*
+				*	#### @property noNavigation::bar::components
+				*
+				*	Each bar can have one or more components.  Currently supported
+				*	component types are `button` and `message`. Each component
+				*	is rendered, events wired up and then appended to its navbar.
+				*/
 				for(var c in bar.components) {
 					var comp = bar.components[c],
 						tmpl = templateFactories[comp.type],
-						btn = tmpl(ctx, comp, scope, el);
+						renderedComp = tmpl(ctx, comp, scope, el);
 
-					btn.click(_click.bind(ctx, ctx, comp, scope, el));
+					//JAG 1/7/2017 - moved this to the button template factory above.
+					//btn.click(_click.bind(ctx, ctx, comp, scope, el));
 
-					btnBar.append(btn);
+					btnBar.append(renderedComp);
 				}
 
 				el.append(btnBar);
 			}
 
+			/*
+			*	#### @property noNavigation::scopeKey
+			*
+			*	noNavigation keeps track of the currently visible navbar using
+			*	an object called `noNavigation` that is store on Angular's scope.
+			*	The `scopeKey` property is used to uniquely identify a given
+			*	noNavigation instance on that noNavigation scope object.
+			*
+			*	noNavigation currently supports two distinct ways of using the
+			*	scopeKey property; either stand-alone or in conjunction with
+			*	a KendoUI Grid's RowUID.
+			*/
 			if(ctx.component.useKendoRowDataUid) {
+				/*
+				*	#### @property noNavigation::useKendoRowDataUid
+				*
+				*	This property is used when you want to use a noNavigation directive
+				*	in a KendoUI Grid's column template. When this property is set to `true`
+				*	the rows `UID` is included in the `scopeKey` for the noNavigation
+				*	directive.
+				*
+				*	> Because AngularJS scopeKey don't like hyphens in their names,
+				*	> they are replaced with underscores.
+				*/
 				var uid =  noKendoHelpers.getGridRowUID(el).replace(/-/g, "_");
 
-				noInfoPath.setItem(scope, "noNavigation." + ctx.component.scopeKey + "_" + uid + ".currentNavBar", ctx.component.default);
+				_registerWatch(ctx, scope, el, uid);
 
-				var unRegWatch = scope.$watch("noNavigation." + ctx.component.scopeKey + "_" + uid + ".currentNavBar", noKendoHelpers.changeRowNavBarWatch.bind(ctx, ctx, scope, el));
-
-				noInfoPath.setItem(scope, "noNavigation." + ctx.component.scopeKey + "_" + uid + ".deregister", unRegWatch);
-
+				/*
+				*	Due to the specific behavior of KendoUI Grid, and inline row editing,
+				*	a way of detecting when they add and remove elements from a table
+				*	was needed.  Using the HTML5 `MutationObserver` accomplishes this
+				*	design goal. This is done by getting a reference to the `TBODY`
+				*	element that contains the grid rows, which will be the observers
+				*	"target." A new `MutationObserver` object is created giving it
+				*	a callback method that will provide an array of mutations. Each
+				*	mutation has a list of removed nodes and a list of added nodes.
+				*/
 				var target = noKendoHelpers.getGridRow(el).parent()[0];
 
 				// create an observer instance
@@ -134,6 +243,12 @@
 					for(var m=0; m<mutations.length;m++) {
 						var mutation = mutations[m];
 
+						/*
+						*	Each removed node is retrieved from the noNavigation scope object
+						*	and if it has a `deregister` method, it is called and then
+						*	navbar that identifies with the remove node is deleted from
+						*	the scope object.
+						*/
 						for(var n=0; n<mutation.removedNodes.length;n++) {
 							var uid = noInfoPath.toScopeSafeGuid(mutation.removedNodes[n].attributes["data-uid"].value),
 								non = noInfoPath.getItem(scope, "noNavigation"),
@@ -146,11 +261,17 @@
 								}
 						}
 
+						/*
+						*	Each node added to the grid is recompiled using `$compile` via
+						*	the `noKendoHelpers.ngCompileSelectedRow` method. Then the
+						*	watch is established for the new row.
+						*/
 						for(var n1=0; n1<mutation.addedNodes.length;n1++) {
-							var uidN = noInfoPath.toScopeSafeGuid(mutation.addedNodes[n1].attributes["data-uid"].value),
-								nonN = noInfoPath.getItem(scope, "noNavigation");
+							var uidN = noInfoPath.toScopeSafeGuid(mutation.addedNodes[n1].attributes["data-uid"].value);
 
 							noKendoHelpers.ngCompileSelectedRow(ctx, scope, el, "noGrid");
+
+							_registerWatch(ctx, scope, el, uidN);
 						}
 					}
 				}.bind(ctx, ctx, scope, el));
