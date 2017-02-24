e66922b4afd35e1f8fb59e3928406670ca00c2cd
Jeffrey A. Gochin <jeff@imginconline.com>
2017-02-24 17:44:20 -0500

    bumped version to 2.0.24

--- docs/noinfopath-forms.md
+++ docs/noinfopath-forms.md
@@ -1,5 +1,5 @@
 # noinfopath.forms
-@version 2.0.23
+@version 2.0.24
 
 Implements the NoInfoPath Transaction processing in conjunction with AngularJS validation mechanism.
 
--- src/globals.js
+++ src/globals.js
@@ -1,6 +1,6 @@
 /**
  * # noinfopath.forms
- * @version 2.0.23
+ * @version 2.0.24
  *
  * Implements the NoInfoPath Transaction processing in conjunction with AngularJS validation mechanism.
  *
